7b285dbffb058ed4fd19272844ab3f22e19512b7
Yixin JIn <jinyixin@umich.edu>
2023-06-28 23:50:16 -0400

    check wheather face points in hair

--- diffuser.py
+++ diffuser.py
@@ -49,12 +49,42 @@ def get_hair_mask(image_path):
     face_detector = facer.face_detector('retinaface/mobilenet', device=device)
     with torch.inference_mode():
         faces = face_detector(image)
+    face_aligner = facer.face_aligner('farl/ibug300w/448', device=device) # optional: "farl/wflw/448", "farl/aflw19/448"
+    with torch.inference_mode():
+        faces_a = face_aligner(image, faces)
+    img = cv2.imread(image_path)[..., ::-1]
+    #vis_img = img.copy()
+    for pts in faces_a['alignment']:
+        pts = pts.cpu().numpy()
+        left_half_face = pts[2:8,:]
+        right_half_face = pts[9:15,:]
+        jaw_point_y = int(pts[8:9,:][0][1])
+        print(jaw_point_y)
+        middle_face_y = pts[[2,14],:]
+        middle_face_y = int((middle_face_y[0][1] + middle_face_y[1][1])//2)
+        print(middle_face_y)
+        face_left_point = pts[0:1,:]
+        face_right_point = pts[16:17,:]
+        face_horizontal_size = face_right_point[0][0] - face_left_point[0][0]
+        pts = pts[9:15,:]
+        divider = int(face_horizontal_size // 25)
+        left_half_face[:,0] = left_half_face[:,0] + divider
+        right_half_face[:,0] = right_half_face[:,0]- divider
+        face_pts_rows = left_half_face[:,1].astype(int).tolist() + right_half_face[:,1].astype(int).tolist()
+        face_pts_cols = right_half_face[:,0].astype(int).tolist() + right_half_face[:,0].astype(int).tolist()
+        #vis_img = facer.draw_landmarks(vis_img, None, pts)
+    
     face_parser = facer.face_parser('farl/lapa/448', device=device) # optional "farl/celebm/448"
     with torch.inference_mode():
         faces = face_parser(image, faces)
     seg_logits = faces['seg']['logits']
     seg_probs = seg_logits.softmax(dim=1)  # nfaces x nclasses x h x w
+    #n_classes = seg_probs.size(1)
     vis_seg_probs = seg_probs.argmax(dim=1)#/n_classes*255
+    # vis_seg_probs_new = vis_seg_probs/n_classes*255
+    # vis_img_new = vis_seg_probs_new.sum(0, keepdim=True)
+    # facer.show_bhw(vis_img_new)
+    # facer.show_bchw(facer.draw_bchw(image, faces))
 
     #Get eye brow mask
     eye_brow_mask = get_specific_mask(vis_seg_probs, 2, 3)
@@ -71,21 +101,22 @@ def get_hair_mask(image_path):
     face_pixel = np.count_nonzero(lower_face_mask == 255)
     #Get hair mask
     hair_mask = get_specific_mask(vis_seg_probs, 10, 10)
-    #Get hair mask
-    image_height = hair_mask.shape[1]
+    #Check wheather 12 face points r in hair region
+    face_points = np.zeros_like(hair_mask)
+    face_points[:,face_pts_rows,face_pts_cols] = 1
+    face_points_in_hair = face_points * hair_mask
+    face_points_in_hair = np.count_nonzero(face_points_in_hair == 255)
+    if face_points_in_hair > 6:
+        print(f"face points in hair is: {face_points_in_hair} >6, skip")
+        return None
     hair_count = np.count_nonzero(hair_mask == 255)
-    lower_hair_count = np.count_nonzero(hair_mask[:,(2*image_height//3):,:] == 255)
+    lower_hair_count = np.count_nonzero(hair_mask[:,middle_face_y:jaw_point_y,:] == 255)
     lower_hair_pp = lower_hair_count / hair_count
-    dilate_kernal_size = int(math.sqrt(face_pixel)) // 20
-    print(f'hair_dilate_size is {int(math.sqrt(hair_count))//10}')
-    print(f'face dilate size is {int(math.sqrt(face_pixel))//20}')
-    print(dilate_kernal_size)
+    dilate_kernal_size = int(math.sqrt(face_pixel)) // divider
     if lower_hair_pp > 0.1:
-        face_erode_size = int(math.sqrt(face_pixel))//6
-        mask_dilate_size = int(math.sqrt(hair_count))//20 #dilate_kernal_size
+        mask_dilate_size = int(math.sqrt(hair_count))//divider #dilate_kernal_size
     else:
-        mask_dilate_size = int(math.sqrt(hair_count))//10 + dilate_kernal_size 
-        #mask_dilate_size = int(hair_over_img * image_shortest_edge // 3 + dilate_kernal_size)
+        mask_dilate_size = int(math.sqrt(hair_count))//(divider//2) + dilate_kernal_size 
     mask_img = np.expand_dims(dilate_mask(np.squeeze(hair_mask[0]), mask_dilate_size), 0)
     eye_mask = np.expand_dims(dilate_mask(np.squeeze(eye_mask[0]), dilate_kernal_size//2), 0)
     lower_face_mask = np.expand_dims(erode_mask(np.squeeze(lower_face_mask[0]), int(2* dilate_kernal_size)), 0)
@@ -97,7 +128,6 @@ def get_hair_mask(image_path):
     mask_img = Image.fromarray(mask_img[0])
     mask_img = mask_img.convert("L")
     mask_img = mask_img.filter(ImageFilter.GaussianBlur(dilate_kernal_size//2))
-    #mask_img.show()
     return mask_img
 
 def make_inpaint_condition(image, image_mask):
@@ -139,6 +169,8 @@ def sd_controlnet_inpaint(init_image, mask_image):
 def generate_final_result(image_path):
     init_image = Image.open(image_path).convert("RGB").resize((WIDTH, HEIGHT))
     mask_image = get_hair_mask(image_path)
+    if mask_image == None:
+        return None
     mask_image = mask_image.resize((WIDTH, HEIGHT))
     result_image = sd_controlnet_inpaint(init_image, mask_image)
     mask = np.array(mask_image.resize((WIDTH, HEIGHT)))/255.0
@@ -154,6 +186,8 @@ if __name__=="__main__":
     for image_file in image_files:
         print(image_file)
         result =  generate_final_result(image_file)
+        if result == None:
+            continue
         file_name = os.path.basename(image_file)
         result_file_name = os.path.join(args.output_path, file_name)
         result.save(result_file_name)
